9077bcf2f082113b342fe223bcc6f9d7063d2aca
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-01-06 16:14:00 +0100

    upadte

--- frontend/src/pages/ProjectDetails/Details/ButtonAddBug.js
+++ frontend/src/pages/ProjectDetails/Details/ButtonAddBug.js
@@ -1,11 +1,12 @@
 import { Container, Button ,Box } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 export const ButtonAddBug = () => {
+  const {id} = useParams()
 
   return (
     <Box display="start" justifyContent="" mb={2}>
-      <Link to={'/new-bug'} style={{ textDecoration: 'none' }}>
+      <Link to={process.env.REACT_APP_API_URL + `#/project/${id}/new-bug`} style={{ textDecoration: 'none' }}>
         <Button
           className="rounded-pill"
           color="primary"
