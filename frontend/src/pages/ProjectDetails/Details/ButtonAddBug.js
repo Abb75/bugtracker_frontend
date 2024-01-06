8878a1b6474c42d0deb956ea0683f6fd512c9145
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-01-06 16:47:06 +0100

    upadte

--- frontend/src/pages/ProjectDetails/Details/ButtonAddBug.js
+++ frontend/src/pages/ProjectDetails/Details/ButtonAddBug.js
@@ -6,7 +6,7 @@ export const ButtonAddBug = () => {
 
   return (
     <Box display="start" justifyContent="" mb={2}>
-      <Link to={process.env.REACT_APP_API_URL + `#/project/${id}/new-bug`} style={{ textDecoration: 'none' }}>
+      <Link to={`https://abb75.github.io/bugtracker_frontend/#/project/${id}/new-bug`} style={{ textDecoration: 'none' }}>
         <Button
           className="rounded-pill"
           color="primary"
