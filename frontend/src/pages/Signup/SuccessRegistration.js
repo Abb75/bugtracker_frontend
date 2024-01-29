81fef206a64107750cda1918ef6f53b608090433
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-01-29 12:13:53 +0100

    update

--- frontend/src/pages/Signup/SuccessRegistration.js
+++ frontend/src/pages/Signup/SuccessRegistration.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios'; // Assurez-vous d'importer axios
 import { useNavigate } from 'react-router-dom';
 
 const RegistrationCongratulations = () => {
-  const navigate = useNavigate()
   const { id } = useParams();
 
   const sendConfirmRegistration = async () => {
@@ -17,23 +16,26 @@ const RegistrationCongratulations = () => {
         }
       };
 
-      // Utilisation de la méthode axios.post correctement
+    
       await axios.patch(`${process.env.REACT_APP_API_URL}/register-confirm/${id}/`, { is_active: true }, config);
 
-      // Redirection vers la page de connexion après la confirmation
-      navigate('/login');
+     
     } catch (error) {
       console.error('Erreur lors de la confirmation d\'inscription :', error);
-      // Gérez les erreurs selon vos besoins
     }
   };
+
+  useEffect(() => {
+        sendConfirmRegistration()
+  
+  }, [])
 
   return (
     <div>
       <h1>Félicitations !</h1>
       <p>Votre inscription est confirmée.</p>
       <p>Vous pouvez maintenant vous connecter en utilisant le lien ci-dessous :</p>
-      <Link onClick={() => sendConfirmRegistration()} to="/login">Page de connexion</Link>
+      <Link  to="/login">Page de connexion</Link>
     </div>
   );
 };
