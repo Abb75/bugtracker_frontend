353f1d448ff8d040d17a5167131d061df7b5be43
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-03-28 17:05:49 +0100

    update

--- frontend/src/pages/ArchivedBug/BugList.js
+++ frontend/src/pages/ArchivedBug/BugList.js
@@ -4,12 +4,20 @@ import {useEffect, useState} from 'react';
 import { Container, Typography, Card, CardContent, Button , MenuItem, IconButton, Grid, Menu} from '@mui/material';
 import { useDispatch } from 'react-redux';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { SendSuccessNotification } from '../../components/Alert';
 import { GetCurrentUser, GetTokenUser } from '../../redux/selectors/userSelectors';
 import { GetBugArchivedApi } from '../../redux/actions/bugActions';
 import { UpdateBugArchivedApi } from '../../redux/actions/bugActions';
 import { GetBugArchived } from '../../redux/selectors/bugSelectors';
-import DeleteBugDialog from '../../components/dialog/DeleteBug';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { DeleteBugApi ,GetBugProjectApi } from '../../redux/actions/bugActions';
+
+
 
 export const ArchivedBug = () => {
   const currentUser = GetCurrentUser()
@@ -18,6 +26,35 @@ export const ArchivedBug = () => {
   const [bugs, setBugs] = useState([])
   const bugsArchived = GetBugArchived()
   const [anchorEl, setAnchorEl] = useState({});
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const [bugIdToDelete, setBugIdToDelete] = useState(null);
+  const [projectId, setProjectId] = useState(null)
+
+
+
+
+
+  const handleDeleteConfirmationClose = async (confirmed) => {
+    if (confirmed) {
+      try { 
+        await  DeleteBugApi(bugIdToDelete, projectId, tokenUser)
+        dispatch(GetBugProjectApi(projectId, tokenUser))
+        dispatch(GetBugArchivedApi(tokenUser))
+        SendSuccessNotification('Bug delete with success !')
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    setDeleteConfirmationOpen(false);
+  };
+
+  const handleDeleteBug = async (bug, project) => {
+    setBugIdToDelete(bug)
+    setProjectId(project);
+    setDeleteConfirmationOpen(true);
+  };
 
      
   const handleClick = (event, projectId) => {
@@ -43,7 +80,8 @@ export const ArchivedBug = () => {
       SendSuccessNotification('Bug unarchived with success')
       dispatch(GetBugArchivedApi(tokenUser))
     }catch(error){
-      throw error    }
+      console.error(error)
+    }
    
     handleClose();
   };
@@ -56,7 +94,7 @@ export const ArchivedBug = () => {
 
     }
     catch(error){
-      throw error
+      console.error(error)
     }
   }, [tokenUser] )
 
@@ -104,11 +142,17 @@ export const ArchivedBug = () => {
               </Grid>
 
               <Grid>
-              {currentUser.groups[0] === 'admin' ? ( 
-                    <DeleteBugDialog bug={bug.id} project={bug.project_id} />) :
-                  (null)}
               
-               
+              <Grid>
+                <Button
+                  onClick={() => handleDeleteBug(bug.id, bug.project_id)}
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<DeleteIcon />}
+                >
+                  Delete
+                </Button>
+              </Grid>
               </Grid>
              
             </Grid>
@@ -133,6 +177,29 @@ export const ArchivedBug = () => {
           No archived bugs at the moment.
         </Typography>
       )}
+
+
+
+      
+<Dialog
+        open={deleteConfirmationOpen}
+        onClose={() => handleDeleteConfirmationClose(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete projet definitely?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Your bug will definitely be deleted.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleDeleteConfirmationClose(false)}>Disagree</Button>
+          <Button onClick={() => handleDeleteConfirmationClose(true)} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
 
 
