4991ace41232a24d31a65e58f09d926f2c194deb
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-04-25 18:18:41 +0200

    update

--- frontend/src/pages/Dashboard/Box/AllProjects.js
+++ frontend/src/pages/Dashboard/Box/AllProjects.js
@@ -11,8 +11,8 @@ export const BoxCountProject = () => {
     return(
 
 <Box 
-           sx={{ backgroundColor: '#EF963C',}}   display="flex" justifyContent="flex" marginTop="100px" paddingRight="30px" >
-<Paper elevation={4} sx={{marginLeft: '3px', marginBottom:'10px', width: '350px', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'primary.contrastText',  boxShadow: '1px 1px 7px rgba(0.0, 0.0, 0.0, 0.1)' }}>
+            display="flex" justifyContent="flex" marginTop="100px" paddingRight="30px" >
+<Paper elevation={4} sx={{ backgroundColor: '#F0A04F', marginLeft: '3px', marginBottom:'10px', width: '350px', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'primary.contrastText',  boxShadow: '1px 1px 7px rgba(0.0, 0.0, 0.0, 0.1)' }}>
   <Box textAlign="center">
     <Typography color={'blue'} variant="h4" component="h2">
      {filterProjectData?.length ??  '0'}
