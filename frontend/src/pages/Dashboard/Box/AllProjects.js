0b28b178c634492bc7ec5563b49d9463d7645e88
Abb75 <88087502+Abb75@users.noreply.github.com>
2024-04-25 18:24:21 +0200

    update

--- frontend/src/pages/Dashboard/Box/AllProjects.js
+++ frontend/src/pages/Dashboard/Box/AllProjects.js
@@ -14,7 +14,7 @@ export const BoxCountProject = () => {
             display="flex" justifyContent="flex" marginTop="100px" paddingRight="30px" >
 <Paper elevation={4} sx={{ backgroundColor: '#F2B16F', marginLeft: '3px', marginBottom:'10px', width: '350px', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'primary.contrastText',  boxShadow: '1px 1px 7px rgba(0.0, 0.0, 0.0, 0.1)' }}>
   <Box textAlign="center">
-    <Typography color={'blue'} variant="h4" component="h2">
+    <Typography color={'white'} variant="h4" component="h2">
      {filterProjectData?.length ??  '0'}
     </Typography>
  
--- frontend/src/pages/Dashboard/Box/AllTickets.js
+++ frontend/src/pages/Dashboard/Box/AllTickets.js
@@ -25,7 +25,7 @@ export const BoxCountTickets = () => {
 <Box display="flex" justifyContent="center" marginLeft="-170px" marginTop='40px' style={{paddingRight: '15px', paddingLeft: '5px'}}>
 <Paper elevation={4} sx={{backgroundColor: '#F2B16F',marginLeft: '180px',marginBottom: '-50px', width: '345px', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'primary.contrastText',  boxShadow: '1px 1px 7px rgba(0.0, 0.0, 0.0, 0.1)' }}>
   <Box textAlign="center">
-  <Typography color={'blue'} variant="h4" component="h2">
+  <Typography color={'white'} variant="h4" component="h2">
      {projectfilter?.length ??  '0'}
     </Typography>
  
